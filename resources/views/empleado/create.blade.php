34e8ba26c6d8e8680fb6cbeebe65a9cf575fdfc0
Jorge R Zegarra Mendoza <jorge101998ronaldo@gmail.com>
2023-11-06 23:23:31 -0500

    form empleados

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,1 +1,21 @@
-Formulario de creacion de empleados
+
+<form action="">
+
+</form>
+<table class="table table-light">
+        <thead class="thead-light">
+            <tr>
+                <th>#</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>#</th>
+            </tr>
+        </tfoot>
+    </table>
